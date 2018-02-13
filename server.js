5fb06b0ec76a609b4419dba76b29be4a2132ac12
Alex Green <alex@devfarms.com>
2018-02-12 18:00:17 -0800

    Cannot connect to database.

--- server.js
+++ server.js
@@ -1,11 +1,67 @@
 // Dependencies
-var express = require("express");
+var express = require('express');
+var mongojs = require('mongojs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 // Initialize Express
 var app = express();
 
+// Database configuration
+// var databaseUrl = 'scraper';
+// var collections = ['scrapedData'];
+
+var db = mongojs('scraper', ['scrapedData']);
+db.on('error', function(error) {
+    console.log('Database error:', error);
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+});
+
+app.get('/all', function(req, res) {
+    db.scrapedData.find({}, function (err, found) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(found);
+        }
+    });
+});
+
+app.get('/scrape', function(req, res) {
+    
+    request('http://saascoalition.org/saas-blog/', function(error, response, html){
+        var $ = cheerio.load(html);
+
+        $('.entry-title').each(function(i, element) {
+            var title = $(this).children('a').text();
+            var link = $(this).children('a').attr.apply('href');
+            console.log(title);
+
+            if (title && link) {
+                db.scrapedData.save({
+                    title: title,
+                    link: link
+                },
+                function(error, saved) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                    console.log(saved);
+                    }
+                });
+            }
+        });
+    });
+
+    res.send('Scrape complete');
+});
+
 // Set up a static folder (public) for our web app
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // Set the app to listen on port 3000
 app.set("port", (process.env.PORT || 3000));
